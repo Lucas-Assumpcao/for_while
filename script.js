1b46d54ba69d3cb5dab7f71fe520af5bb046c453
Lucas-Assumpcaogit config --global user.email assumpdev@gmail.comgit config --global user.name Lucas-Assumpcao <assumpdev@gmail.com>
2026-04-10 22:06:09 -0300

    for_while

---
+++ script.js
@@ -0,0 +1,50 @@
+//for(let contador = 1; contador <= 10; contador++) {
+//    console.log(contador);
+//}
+
+
+const frutas = ["maçã", "banana", "laranja", "uva", "abacaxi"];
+
+//console.log(frutas.length);
+
+const cache = frutas.length;
+
+//for (let contador = 0; contador < cache; contador ++){
+//    console.log(frutas[contador]);
+//}
+
+//for(const fruta of frutas) {
+//    console.log(fruta);
+//}
+
+// for (let contador = 0; contador < cache; contador ++){
+//     if (contador % 2 == 0) {
+//         console.log(frutas[contador]);
+//     }
+// }
+
+// const clientes = {
+//     nome: "João",
+//     idade: 30,
+//     cidade: "Osasco",
+// };
+// for(const dados in clientes){
+//     console.log(clientes[dados]);
+// }
+
+// let contador = 10;
+// while ( contador >= 0) {
+//     console.log(contador);
+//     contador--;
+// }
+
+console.log("OK");
+
+let entradausuario = +prompt("Digite um número:");
+
+
+
+
+while (entradausuario != 2) {
+    console.log((entradausuario = +prompt("Digite um número:")));
+}
