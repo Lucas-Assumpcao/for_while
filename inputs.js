ca2add4c4e6bad6722e685fe680bc690dbde6c87
Lucas-Assumpcaogit config --global user.email assumpdev@gmail.comgit config --global user.name Lucas-Assumpcao <assumpdev@gmail.com>
2026-04-17 21:44:59 -0300

    add

--- inputs.js
+++ inputs.js
@@ -1,13 +1,25 @@
 const elemento = {
     input:document.querySelector ('#color'),
     container : document.querySelector('#container'),
+    inputs:document.querySelector('#text'),
 };
 
-console.log(elemento);
+console.log(elemento.inputs);
+
 
 elemento.input.addEventListener('input', (evento) => {
     console.log(evento.currentTarget.value);
+    
 
     elemento.container.style.backgroundColor = evento.currentTarget.value;
+
+});
+
+elemento.inputs.addEventListener('input', (evento) => {
+    console.log(evento.currentTarget.value);
+
+    
+
+    elemento.container.innerText = evento.currentTarget.value;
 
 });
